200cddc6cce3451be750377216d9e15322cad955
fzksbalazs <fzksbalazs@gmail.com>
2024-09-11 16:58:45 +0200

    hibak javitasa

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // Elirás
-    std::cout << '1-100 ertekek duplazasa'; //String rosszul van kiirva
-    for (int i = 0;) // Ciklus feltétel hiányos
+    int *b = new int[N_ELEMENTS]; // Elirás
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //String rosszul van kiirva
+    for (int i = 0; i < N_ELEMENTS; i++) // Ciklus feltétel hiányos
     {
-        b[i] = i * 2; // i+1 hogy 1től kezdődjön a duplázás
+        b[i] = (i + 1) * 2;  // i+1 hogy 1től kezdődjön a duplázás
     }
-    for (int i = 0; i; i++) // Ciklus feltétel javitása
+
+     for (int i = 0; i < N_ELEMENTS; i++) // Ciklus feltétel javitása
     {
-        std::cout << "Ertek:" //Hiányzó kiirás  + bezárás
-    }    
+          std::cout << "Ertek: " << b[i] << std::endl; //Hiányzó kiirás  + bezárás
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // kezdeti ertekadas
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0; // kezdeti ertekadas
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // ; hiánya
+        atlag += b[i]; // ; hiánya
     }
-    atlag /= N_ELEMENTS; 
+    atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
