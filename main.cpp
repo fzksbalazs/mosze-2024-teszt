41da0804f5cc97e01058808b541f676b81719bde
fzksbalazs <fzksbalazs@gmail.com>
2024-09-11 16:48:06 +0200

    Hibák!

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Elirás
+    std::cout << '1-100 ertekek duplazasa'; //String rosszul van kiirva
+    for (int i = 0;) // Ciklus feltétel hiányos
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // i+1 hogy 1től kezdődjön a duplázás
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // Ciklus feltétel javitása
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Hiányzó kiirás  + bezárás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; // kezdeti ertekadas
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiánya
     }
-    atlag /= N_ELEMENTS;
+    atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
